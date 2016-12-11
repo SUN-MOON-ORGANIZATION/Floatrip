93342749fc2a82958926983349f0d4e9971fce04
nisikn <kouki1121pen@gmail.com>
2016-12-11 16:01:33 +0900

    BGMManager,SEManager

---
+++ Floatrip_master/Assets/Scripts/BGMManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMManager : MonoBehaviour {
+
+	public AudioSource introBGM;
+	public AudioSource loopBGM;
+	AudioSource audioSource;
+	
+	// Use this for initialization
+	void Start () {
+		introBGM = GameObject.Find("intro").GetComponent<AudioSource> ();
+		loopBGM = GameObject.Find ("loop").GetComponent<AudioSource> ();
+
+		introBGM.Play();//始まった時イントロのBGMを流す
+		Invoke ("DelayBGM", 30.1f);//30秒のイントロ流れた後ループBGMを流す
+	}
+	void DelayBGM(){
+		loopBGM.Play ();
+	}
+		
+}
