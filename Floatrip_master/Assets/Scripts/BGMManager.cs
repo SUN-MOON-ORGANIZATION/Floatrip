2fecd66a2311baa1c11d86d68fa8caf40277bfdb
nisikn <kouki1121pen@gmail.com>
2016-12-18 12:15:30 +0900

    BGMいれました
    SEできてません

--- Floatrip_master/Assets/Scripts/BGMManager.cs
+++ Floatrip_master/Assets/Scripts/BGMManager.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class BGMManager : MonoBehaviour {
 
 	public AudioSource introBGM;
 	public AudioSource loopBGM;
 	AudioSource audioSource;
+	string sceneName;
 	
 	// Use this for initialization
 	void Start () {
-		introBGM = GameObject.Find("intro").GetComponent<AudioSource> ();
-		loopBGM = GameObject.Find ("loop").GetComponent<AudioSource> ();
+		sceneName = SceneManager.GetActiveScene ().name;
+		introBGM = GameObject.Find("IntroBGM").GetComponent<AudioSource> ();
+		loopBGM = GameObject.Find ("LoopBGM").GetComponent<AudioSource> ();
 
 		introBGM.Play();//始まった時イントロのBGMを流す
-		Invoke ("DelayBGM", 30.1f);//30秒のイントロ流れた後ループBGMを流す
+		if(sceneName=="Title"){
+			Invoke ("DelayBGM", 30.0f);//30秒のイントロ流れた後ループBGMを流す
+		}
+		if (sceneName == "Main") {
+			Invoke ("DelayBGM", 80.0f);//80秒のイントロが流れた後ループBGMを流す
+		}
 	}
 	void DelayBGM(){
 		loopBGM.Play ();
